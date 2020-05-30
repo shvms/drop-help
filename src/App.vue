0b6c2af75c89ef65b6091527be76db3a0ec2a6ae
Shuvam Shah <shvmks@outlook.com>
2020-05-30 16:49:27 +0530

    Hero container added

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
   <div id="app">
+    <HeroContainer />
+
     <div class="container">
       <router-view :database="database" />
     </div>
@@ -8,9 +10,13 @@
 
 <script>
 import db from "./database.json";
+import HeroContainer from "@/components/HeroContainer";
 
 export default {
   name: "App",
+  components: {
+    HeroContainer,
+  },
   data() {
     return {
       database: db,
@@ -19,4 +25,16 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+.jumbotron {
+  background-color: #eff8f5;
+  background-image: url("../src/assets/bg.jpg");
+  background-position: right center;
+  background-repeat: no-repeat;
+  background-size: contain;
+  height: 60vh;
+
+  display: flex;
+  align-items: center;
+}
+</style>
---
+++ src/components/HeroContainer.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="jumbotron jumbotron-fluid" ref="jumbotron">
+    <div class="container">
+      <h1 class="display-3">DropHelp</h1>
+      <p class="lead">
+        An open source initiative to drive more awareness towards local NGOs.
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HeroContainer",
+};
+</script>
