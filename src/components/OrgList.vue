528560c4d27f25d3149bb41c5adc04b67d87897c
Shuvam Shah <shvmks@outlook.com>
2020-06-02 18:00:18 +0530

    Separate Organisation list component

---
+++ src/components/OrgList.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="row row-cols-1 row-cols-md-3">
+    <div class="col mb-4" v-for="org in orgs" :key="org.id">
+      <div class="card h-100 shadow">
+        <div class="card-body">
+          <h5 class="card-title" style="font-family: 'Poppins', sans-serif;">{{ org.title }}</h5>
+          <p class="card-text">
+            <span v-for="(concern, i) in org.concerns" :key="i">
+              {{ concern.topic }}
+              <span v-if="i != org.concerns.length - 1">&bull;</span>
+            </span>
+          </p>
+          <p class="card-text text-muted">
+            Locations Serving:
+            <span v-for="(loc, i) in org.locationServingAt" :key="i">
+              {{ loc.city }}
+              <span v-if="i != org.locationServingAt.length - 1">&bull;</span>
+            </span>
+          </p>
+          <button type="button" class="btn btn-dark mr-1">Pay via GPay</button>
+          <router-link
+            :to="{
+                name: 'Details',
+                params: { slug: org.slug },
+              }"
+          >
+            <button type="button" class="btn btn-dark mr-1">More Info</button>
+          </router-link>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "OrgList",
+  props: {
+    orgs: {
+      type: Array,
+      required: true
+    }
+  }
+};
+</script>
+
+<style scoped>
+.form-control::placeholder {
+  font-size: 0.95rem;
+  color: #aaa;
+  font-style: italic;
+}
+
+.card {
+  transition: 0.3s ease all;
+}
+
+.card:hover {
+  transform: translateY(-5px);
+  transition: 0.3s ease all;
+}
+
+.btn {
+  font-weight: 600;
+  background: #000;
+}
+
+.btn:hover {
+  background-color: #343a40;
+}
+</style>
--- src/views/Index.vue
+++ src/views/Index.vue
@@ -2,84 +2,50 @@
   <div>
     <SearchBar @search="fuzzySearch" />
 
-    <div class="row row-cols-1 row-cols-md-3">
-      <div class="col mb-4" v-for="org in actualDB" :key="org.id">
-        <div class="card h-100 shadow">
-          <div class="card-body">
-            <h5 class="card-title" style="font-family: 'Poppins', sans-serif;">
-              {{ org.title }}
-            </h5>
-            <p class="card-text">
-              <span v-for="(concern, i) in org.concerns" :key="i">
-                {{ concern.topic }}
-                <span v-if="i != org.concerns.length - 1">&bull;</span>
-              </span>
-            </p>
-            <p class="card-text text-muted">
-              Locations Serving:
-              <span v-for="(loc, i) in org.locationServingAt" :key="i">
-                {{ loc.city }}
-                <span v-if="i != org.locationServingAt.length - 1">&bull;</span>
-              </span>
-            </p>
-            <button type="button" class="btn btn-dark mr-1">
-              Pay via GPay
-            </button>
-            <router-link
-              :to="{
-                name: 'Details',
-                params: { slug: org.slug },
-              }"
-            >
-              <button type="button" class="btn btn-dark mr-1">
-                More Info
-              </button>
-            </router-link>
-          </div>
-        </div>
-      </div>
-    </div>
+    <OrgList :orgs="actualDB" />
   </div>
 </template>
 
 <script>
 import SearchBar from "@/components/SearchBar";
+import OrgList from "@/components/OrgList";
 import * as JsSearch from "js-search";
 
 export default {
   name: "Index",
   components: {
     SearchBar,
+    OrgList
   },
   props: {
     database: {
       type: Array,
-      required: true,
-    },
+      required: true
+    }
   },
   data() {
     return {
       results: this.database,
-      jssearch: new JsSearch.Search("id"),
+      jssearch: new JsSearch.Search("id")
     };
   },
   methods: {
     fuzzySearch(needle) {
       this.results = this.jssearch.search(needle);
       console.log(this.results.length);
-    },
+    }
   },
   computed: {
     actualDB() {
       return this.results.length > 0 ? this.results : this.database;
-    },
+    }
   },
   mounted() {
     this.jssearch.addIndex("title");
     this.jssearch.addIndex(["locationServingAt", "city"]);
     this.jssearch.addIndex(["concerns", "tags"]);
     this.jssearch.addDocuments(this.database);
-  },
+  }
 };
 </script>
 
@@ -93,21 +59,6 @@ export default {
   border-bottom-width: 1px;
   border-radius: 0;
   padding-left: 0;
-}
-
-.form-control::placeholder {
-  font-size: 0.95rem;
-  color: #aaa;
-  font-style: italic;
-}
-
-.card {
-  transition: 0.3s ease all;
-}
-
-.card:hover {
-  transform: translateY(-5px);
-  transition: 0.3s ease all;
 }
 
 * {
